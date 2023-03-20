64f952b343410400e49627deb2f566ccfb2f57fd
cgriungu <kristinekiki65@gmail.com>
2023-03-20 16:19:40 +0300

    db connection added

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://CGRIUNGU:30143926@cluster0.n1tqvez.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://CGRIUNGU:30143926@cluster0.n1tqvez.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://CGRIUNGU:30143926@cluster0.n1tqvez.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://Gakii:Gakii@cluster0.n1tqvez.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://Gakii:Gakii@cluster0.n1tqvez.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://Gakii:Gakii@cluster0.n1tqvez.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
